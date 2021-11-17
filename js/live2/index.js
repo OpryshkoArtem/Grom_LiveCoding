cd8e97541f10cbeb175977394c5274ebbecc21c0
OpryshkoArtem <opryshkoartemua@gmail.com>
2021-11-17 10:49:58 +0200

    add live2

---
+++ js/live2/index.js
@@ -0,0 +1,21 @@
+
+//     PRIME NUMBERS     //
+
+// algo
+// 1. iterate numbers from 2 to num => i
+// 2. iterate from 2 to i => number
+// 3.	если остаток от деления i на number равно 0 - то не простое
+// 4. если на все числа деление с остатком - то число простое, вывести в console.log()
+
+// check 1 - 9
+// 2 -> true
+// 3 (3/2, 3/2, 3/3) -> true
+// 4 (4/1, 4/2 (!), 4/3, 4/4) -> false
+
+// input: number
+// output: underfined
+
+function getPrims(num) {
+	// put your code here
+}
+
