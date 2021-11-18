d20f32e2e91fbdaa2f9dba3b991adf22beac7aff
OpryshkoArtem <opryshkoartemua@gmail.com>
2021-11-18 12:13:11 +0200

    add eslint

--- js/live2/index.js
+++ js/live2/index.js
@@ -1,5 +1,11 @@
-
 //     PRIME NUMBERS     //
+
+// How to work on tech tasks. Step by step guide
+
+// 1. Learn requirement
+// 2. Create step by step algo (& input/output for funtions)
+// 3. Write draft solution & testing
+// 4. Refactoring & final testing -> final solution
 
 // algo
 // 1. iterate numbers from 2 to num => i
@@ -11,11 +17,25 @@
 // 2 -> true
 // 3 (3/2, 3/2, 3/3) -> true
 // 4 (4/1, 4/2 (!), 4/3, 4/4) -> false
-
 // input: number
 // output: underfined
 
-function getPrims(num) {
-	// put your code here
+function isPrime(number) {
+  for (let index = 2; index < number; index += 1) {
+    if (number % index === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
+function getPrimes(num) {
+  for (let i = 2; i <= num; i += 1) {
+    if (isPrime(i)) {
+      console.log(i);
+    }
+  }
+}
+
+// test data
+getPrimes(15);
